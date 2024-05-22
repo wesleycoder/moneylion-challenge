c7396dce2d1f3fb808efeba22ebf71a88d3a2c89
Wésley Guimarães <wes@guima.dev>
2024-05-22 06:41:14 -0300

    feat: add shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 import Link from 'next/link'
+import { getFeed } from '~/server/getFeed'
 
-export default function HomePage() {
+export default async function HomePage() {
+  const feed = await getFeed()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          Create <span className="text-primary">T3</span> App
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <pre className="flex max-w-xs max-h-96 flex-col gap-4 rounded-xl bg-white/10 p-4 overflow-x-auto">
+            {JSON.stringify(feed, null, 2)}
+          </pre>
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
             href="https://create.t3.gg/en/usage/first-steps"
             target="_blank"
           >
@@ -19,7 +24,7 @@ export default function HomePage() {
             </div>
           </Link>
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
             href="https://create.t3.gg/en/introduction"
             target="_blank"
           >
