0d803a2eb0aafc5d53d6de5760d5fa64061be795
Wésley Guimarães <wes@guima.dev>
2024-05-22 02:09:45 -0300

    chore: remove eslint packages
    lint more files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import { type Config } from "drizzle-kit";
+import type { Config } from 'drizzle-kit'
 
-import { env } from "~/env";
+import { env } from '~/env'
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  schema: './src/server/db/schema.ts',
+  dialect: 'sqlite',
   dbCredentials: {
     url: env.DATABASE_URL,
   },
-  tablesFilter: ["moneylion-challenge_*"],
-} satisfies Config;
+  tablesFilter: ['moneylion-challenge_*'],
+} satisfies Config
