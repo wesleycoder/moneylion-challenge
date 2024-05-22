5b8907a9caa079f04838eeb50cf3f3b8dc695ed6
Wésley Guimarães <wes@guima.dev>
2024-05-22 02:09:03 -0300

    chore: init dotenvx

--- src/env.js
+++ src/env.js
@@ -7,8 +7,10 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
+    DATABASE_URL: z.string().url(),
+    FEED_URL: z.string().url(),
+    FEED_IS_DYNAMIC: z.boolean().default(false),
   },
 
   /**
@@ -27,16 +29,9 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    FEED_URL: process.env.FEED_URL,
+    FEED_IS_DYNAMIC: process.env.FEED_IS_DYNAMIC,
   },
-  /**
-   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
-   * useful for Docker builds.
-   */
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-  /**
-   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
-   * `SOME_VAR=''` will throw an error.
-   */
   emptyStringAsUndefined: true,
 })
